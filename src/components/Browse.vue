d2358b1532bfc43b775c6be301f10f25088ed6b8
bakhtiyar_garashov <bakhtiyar.garashov@ut.ee>
2020-11-19 01:31:47 +0200

    Follow buton conditional text added

--- src/components/Browse.vue
+++ src/components/Browse.vue
@@ -1,61 +1,86 @@
 <template>
   <div class="parent">
     <div v-for="user in profiles" :key="user.id" class="profile-container">
-      <img style="width:120px; height:120px; border-radius:100%" :src="user.avatar" alt="avatar" />
-      <div >
-        <h5 >{{ user.firstname }} {{ user.lastname }}</h5>
+      <img
+        style="width: 120px; height: 120px; border-radius: 100%"
+        :src="user.avatar"
+        alt="avatar"
+      />
+      <div>
+        <h5>{{ user.firstname }} {{ user.lastname }}</h5>
 
-        <button @click="user.isFollowed=!user.isFollowed" :class="{followed:user.isFollowed}" >
-          Follow
+        <button
+          @click="user.isFollowed = !user.isFollowed"
+          class="button-normal"
+          :class="{ followed: user.isFollowed }"
+        >
+          <span v-if="user.isFollowed">Following</span>
+          <span v-else-if="!user.isFollowed">Follow</span>
         </button>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
   name: "Browse",
-  
+
   computed: {
     profiles() {
-     
       return this.$store.state.allProfiles;
-
     },
   },
   mounted() {
-   
     this.$store.dispatch("fetchAllProfiles");
   },
-
-
 };
 </script>
 
 <style>
-
-.profile-container{
+.profile-container {
   border: 1px solid #dedede;
   text-align: center;
   border-radius: 20px;
   margin: 20px 0;
   padding: 20px 10px;
   width: 50%;
+  min-width: 50px;
 }
 
-.parent{
+.parent {
   display: flex;
-  
   flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 50px 0;
 }
 
-.followed{
-  background-color: purple;
+.button-normal {
+  border-radius: 10px;
+  color: white;
+  outline: none;
+  border: none;
+  padding: 5px 25px;
+  border: 1px solid #dedede;
+  background-color: #82008f;
 }
 
+.button-normal:focus{
+  outline: none;
+}
+
+.followed {
+  border-radius: 10px;
+  outline: none !important;
+  border: none;
+  padding: 5px 20px;
+  background-color: #ffffff;
+  border: 1px solid #82008f;
+  color: #82008f;
+}
+
+.followed:focus{
+  outline: none;
+}
 </style>
