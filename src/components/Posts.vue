8f2c21602ec967344266625be9abb9ddc3cd0aad
bakhtiyar_garashov <bakhtiyar.garashov@ut.ee>
2020-11-19 09:54:49 +0200

    Click event handlers added (#9)

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,27 +1,41 @@
 <template>
   <div class="parent">
-    <div v-for="post in posts" :key="post.id" class="profile-container">
+    <div v-for="post in posts" :key="post.id" class="post-container">
+      <div class="user-data">
+        <div>
+          <img :src="post.author.avatar" alt="" srcset="" />
+          <p>{{ post.author.firstname }} {{ post.author.lastname }}</p>
+        </div>
+        <p>{{ post.createTime }}</p>
+      </div>
       <img
         v-if="post.media && post.media.type == 'image'"
-        style="width: 120px; height: 120px; border-radius: 100%"
+        style="width: 100%"
         :src="post.media.url"
         alt="avatar"
       />
       <video
-        width="260"
+        style="width: 100%"
         v-if="post.media && post.media.type == 'video'"
         controls
         :src="post.media.url"
       ></video>
       <div>
-        <h5>{{ post.text }}</h5>
+        <h4>{{ post.text }}</h4>
 
         <button
           @click="post.isLiked = !post.isLiked"
           class="button-normal"
           :class="{ liked: post.isLiked }"
         >
-        <span ><img style="margin-bottom:5px;" width="20" src="../assets/like.png" alt="" srcset=""></span>
+          <span
+            ><img
+              style="margin: 3px 0"
+              width="20"
+              src="../assets/like.png"
+              alt=""
+              srcset=""
+          /></span>
           {{ post.likes }}
         </button>
       </div>
@@ -35,6 +49,7 @@ export default {
 
   computed: {
     posts() {
+      console.log(this.$store.getters.posts);
       return this.$store.getters.posts;
     },
   },
@@ -45,31 +60,29 @@ export default {
 </script>
 
 <style scoped>
-.profile-container {
+.post-container {
   border: 1px solid #dedede;
-  text-align: center;
   border-radius: 20px;
-  margin: 20px 0;
   padding: 20px 10px;
-  width: 50%;
+  width: 80%;
   min-width: 50px;
+  margin: 30px 0;
+}
+
+.post-container h4 {
+  margin: 15px 0;
 }
 
 .parent {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
   padding: 50px 0;
 }
 
 .button-normal {
- 
-  border-radius: 10px;
+  border-radius: 5px;
   color: white;
   outline: none;
   border: none;
-  padding: 5px 25px;
+  padding: 5px 10px;
   border: 1px solid #dedede;
   background-color: gray;
 }
@@ -79,15 +92,46 @@ export default {
 }
 
 .liked {
-  
+  border-radius: 5px;
   outline: none !important;
   border: none;
-  padding: 5px 20px;
+  padding: 5px 10px;
   background-color: #01579b;
   color: white;
 }
 
 .liked:focus {
   outline: none;
+}
+
+.user-data {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 10px;
+}
+
+.user-data div {
+  display: flex;
+  flex-direction: row;
+}
+
+.user-data p {
+ margin-top: 3px;
+}
+
+.user-data div img {
+  margin-right: 15px;
+}
+
+.user-data div p {
+  margin-top: 10px;
+}
+
+
+.user-data img {
+  border-radius: 100%;
+  height: 50px;
+  width: 50px;
 }
 </style>
